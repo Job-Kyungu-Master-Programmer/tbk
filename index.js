85ff967f2e49fb4659b7e63b143b222547db3ef3
Job Kyungu <joblodo@Jobs-Mac-mini.local>
2023-12-01 16:45:03 -0800

    ver 1

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+// const cors = require('cors')
+
+// app.use(cors())
+// app.use(express.static('build'))
+
+
+app.get('/', (request, response) => {
+    response.send('<h2>Bonjour</h2>')
+})
+
+const PORT = process.env.PORT || 3002
+app.listen(PORT, () => {
+    console.log(` Server running on PORT ${PORT} `)
+})
